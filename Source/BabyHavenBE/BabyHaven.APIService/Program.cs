8ace48b7f85e16c3e24317b7d2ae8e971ab636bb
huynh <huynhthaitu124@gmail.com>
2025-02-16 08:16:42 +0700

    Deploy web api

--- Source/BabyHavenBE/BabyHaven.APIService/Program.cs
+++ Source/BabyHavenBE/BabyHaven.APIService/Program.cs
@@ -1,4 +1,4 @@
-﻿using BabyHaven.Common.Enum.Converters;
+using BabyHaven.Common.Enum.Converters;
 using Azure;
 using BabyHaven.Repositories;
 using BabyHaven.Repositories.Repositories;
@@ -13,8 +13,11 @@ using OpenAI;
 using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.Caching.Memory;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Environment.EnvironmentName = Environments.Development;
 
 // Đăng ký dịch vụ trong DI Container
 builder.Services.AddScoped<IFeatureService, FeatureService>();
@@ -41,12 +44,34 @@ builder.Services.AddScoped<IChildMilestoneService, ChildMilestoneService>();
 // Đăng ký UnitOfWork và Repository
 builder.Services.AddScoped<UnitOfWork>();
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+        .EnableTokenAcquisitionToCallDownstreamApi()
+            .AddMicrosoftGraph(builder.Configuration.GetSection("MicrosoftGraph"))
+            .AddInMemoryTokenCaches()
+            .AddDownstreamApi("DownstreamApi", builder.Configuration.GetSection("DownstreamApi"))
+            .AddInMemoryTokenCaches();
+var googleClientId = builder.Configuration["Authentication:Google:ClientId"];
+var googleClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+Console.WriteLine($"Google ClientId: {googleClientId}");
+Console.WriteLine($"Google ClientSecret: {googleClientSecret}");
+
+var azureClientId = builder.Configuration["AzureAd:ClientId"];
+var azureSession = builder.Configuration.GetSection("AzureAd");
+Console.WriteLine($"Azure ClientId: {azureSession.GetChildren}");
+
+
+
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
     options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
 });
+
+
+
 
 // Swagger UI
 builder.Services.AddEndpointsApiExplorer();
@@ -78,6 +103,7 @@ builder.Services.AddSwaggerGen(option =>
     });
 });
 
+
 // Cấu hình CORS
 builder.Services.AddCors(options =>
 {
@@ -99,52 +125,66 @@ builder.Services.ConfigureApplicationCookie(options =>
 });
 
 //Cấu hình Authentication
-builder.Services.AddAuthentication(options =>
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Mặc định JWT
+//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Khi cần xác thực, ưu tiên JWT
+//    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme; // Cho Google
+//})
+//.AddCookie(options =>
+//{
+//    options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // Set cookie lifespan to 60 minutes
+//})
+//.AddGoogle(options =>
+//{
+//    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+//    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+//    options.CallbackPath = "/signin-google";
+//});
+////.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+////{
+////    options.TokenValidationParameters = new TokenValidationParameters
+////    {
+////        ValidateIssuer = true,
+////        ValidateAudience = true,
+////        ValidateLifetime = true,
+////        ValidateIssuerSigningKey = true,
+////        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+////        ValidAudience = builder.Configuration["Jwt:Audience"],
+////        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+////    };
+////});
+////.AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("GoogleAuth", policy =>
+//        policy.RequireAuthenticatedUser().AddAuthenticationSchemes(GoogleDefaults.AuthenticationScheme));
+
+//    options.AddPolicy("JwtAuth", policy =>
+//        policy.RequireAuthenticatedUser().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
+//});
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; // Mặc định JWT
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; // Khi cần xác thực, ưu tiên JWT
-    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme; // Cho Google
-})
-.AddCookie(options =>
-{
-    options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // Set cookie lifespan to 60 minutes
-})
-.AddGoogle(options =>
-{
-    options.ClientId = builder.Configuration["Authentication:Google:ClientId"];
-    options.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
-    options.CallbackPath = "/signin-google";
-})
-.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
+    serverOptions.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
+    serverOptions.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(10);
 });
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("GoogleAuth", policy =>
-        policy.RequireAuthenticatedUser().AddAuthenticationSchemes(GoogleDefaults.AuthenticationScheme));
-
-    options.AddPolicy("JwtAuth", policy =>
-        policy.RequireAuthenticatedUser().AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
-});
 
 var app = builder.Build();
 
-//Cấu hình Middleware
-if (app.Environment.IsDevelopment())
+
+
+if(app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "BabyHaven API V1");
+        c.RoutePrefix = string.Empty;
+    });
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
