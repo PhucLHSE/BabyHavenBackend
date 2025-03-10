9c3a99fe36c103396e6c0af0dd138c0b999264f5
Huynh Nguyen Ngoc Nguyen - N1 <Nguyen1.24112003@gmail.com>
2025-03-10 22:39:14 +0700

    Format SpecializationController and SpecializationService

--- Source/BabyHavenBE/BabyHaven.APIService/Controllers/SpecializationsController.cs
+++ Source/BabyHavenBE/BabyHaven.APIService/Controllers/SpecializationsController.cs
@@ -38,7 +38,10 @@ namespace BabyHaven.APIService.Controllers
         {
             if (!ModelState.IsValid)
             {
-                return new ServiceResult(Const.ERROR_VALIDATION_CODE, "Validation failed", ModelState);
+                return new ServiceResult(Const.
+                    ERROR_VALIDATION_CODE,
+                    "Validation failed",
+                    ModelState);
             }
 
             return await _specializationService.Create(specializationCreateDto);
@@ -50,7 +53,10 @@ namespace BabyHaven.APIService.Controllers
         {
             if (!ModelState.IsValid)
             {
-                return new ServiceResult(Const.ERROR_VALIDATION_CODE, "Validation failed", ModelState);
+                return new ServiceResult(Const.
+                    ERROR_VALIDATION_CODE,
+                    "Validation failed",
+                    ModelState);
             }
 
             return await _specializationService.Update(specializationUpdateDto);
