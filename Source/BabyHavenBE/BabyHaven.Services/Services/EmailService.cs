ca51b62ca31256852b4c37034207604a087e369e
Huynh Nguyen Ngoc Nguyen - N1 <Nguyen1.24112003@gmail.com>
2025-03-14 11:34:52 +0700

    Update EmailService.cs

--- Source/BabyHavenBE/BabyHaven.Services/Services/EmailService.cs
+++ Source/BabyHavenBE/BabyHaven.Services/Services/EmailService.cs
@@ -56,41 +56,4 @@ namespace BabyHaven.Services.Services
             }
         }
     }
-
-    //public class EmailService : IEmailService
-    //{
-    //    private readonly string _smtpServer = "smtp.gmail.com";
-    //    private readonly int _smtpPort = 587;
-    //    private readonly string _smtpUsername = "nguyenngoc.24112003@gmail.com";  // Thay bằng email của anh
-    //    private readonly string _smtpPassword = "bsxe bxjs bsdp eqtd";  // Thay bằng mật khẩu ứng dụng
-
-    //    public async Task<bool> SendEmailAsync(string to, string subject, string body)
-    //    {
-    //        try
-    //        {
-    //            using (var client = new SmtpClient(_smtpServer, _smtpPort))
-    //            {
-    //                client.Credentials = new NetworkCredential(_smtpUsername, _smtpPassword);
-    //                client.EnableSsl = true;
-
-    //                var mailMessage = new MailMessage
-    //                {
-    //                    From = new MailAddress(_smtpUsername),
-    //                    Subject = subject,
-    //                    Body = body,
-    //                    IsBodyHtml = true
-    //                };
-    //                mailMessage.To.Add(to);
-
-    //                await client.SendMailAsync(mailMessage);
-    //                return true;
-    //            }
-    //        }
-    //        catch (Exception ex)
-    //        {
-    //            Console.WriteLine($"Email send failed: {ex.Message}");
-    //            return false;
-    //        }
-    //    }
-    //}
 }
