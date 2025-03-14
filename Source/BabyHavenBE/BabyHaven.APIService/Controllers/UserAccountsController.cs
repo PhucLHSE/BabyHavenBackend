9055981f6504ec65c0b14bedf0e602eaec8975b6
PhucLHSE <phuclhse173083@fpt.edu.vn>
2025-03-14 20:14:18 +0700

    style: Format UserAccountsController and UserAccountService

--- Source/BabyHavenBE/BabyHaven.APIService/Controllers/UserAccountsController.cs
+++ Source/BabyHavenBE/BabyHaven.APIService/Controllers/UserAccountsController.cs
@@ -56,6 +56,7 @@ namespace BabyHaven.APIService.Controllers
         {
             if (!ModelState.IsValid)
             {
+
                 return new ServiceResult(Const.
                     ERROR_VALIDATION_CODE,
                     "Validation failed",
@@ -71,6 +72,7 @@ namespace BabyHaven.APIService.Controllers
         {
             if (!ModelState.IsValid)
             {
+
                 return new ServiceResult(Const.
                     ERROR_VALIDATION_CODE,
                     "Validation failed",
