b40127b3ba68ea758e6e95a06efb917fce4503fe
Huynh Nguyen Ngoc Nguyen - N1 <Nguyen1.24112003@gmail.com>
2025-03-21 17:18:52 +0700

    Odata getall Feedback

---
+++ Source/BabyHavenBE/BabyHaven.Common/Enum/RatingFeedbackEnums/RatingFeedbackType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyHaven.Common.Enum.RatingFeedbackEnums
+{
+    public enum RatingFeedbackType
+    {
+        General,         // Đánh giá chung
+        ServiceQuality,  // Chất lượng dịch vụ
+        DoctorAttitude,  // Thái độ bác sĩ
+        ResponseTime,    // Thời gian phản hồi
+        AppExperience,   // Trải nghiệm ứng dụng
+        Other            // Khác
+    }
+}
---
+++ Source/BabyHavenBE/BabyHaven.Services/Mappers/RatingFeedbackMapper.cs
@@ -0,0 +1,37 @@
+﻿using BabyHaven.Common.DTOs.ConsultationResponseDTOs;
+using BabyHaven.Common.DTOs.RatingFeedbackDTOs;
+using BabyHaven.Common.Enum.ConsultationResponseEnums;
+using BabyHaven.Common.Enum.RatingFeedbackEnums;
+using BabyHaven.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabyHaven.Services.Mappers
+{
+    public static class RatingFeedbackMapper
+    {
+        // Mapper RatingFeedbackViewAllDto
+        public static RatingFeedbackViewAllDto MapToRatingFeedbackViewAllDto(this RatingFeedback model)
+        {
+            return new RatingFeedbackViewAllDto
+            {
+                UserId = model.User?.UserId ?? Guid.Empty,
+                ResponseId = model.Response?.ResponseId ?? 0,
+                Rating = model.Rating,
+                Comment = model.Comment,
+                FeedbackDate = model.FeedbackDate,
+                FeedbackType = Enum.TryParse<RatingFeedbackType>(model.FeedbackType, true, out var type)
+                                ? type
+                                : RatingFeedbackType.General,
+
+                // Convert Status from string to enum
+                Status = Enum.TryParse<FeedbackStatus>(model.Status, true, out var status)
+                          ? status
+                          : FeedbackStatus.Pending
+            };
+        }
+    }
+}
