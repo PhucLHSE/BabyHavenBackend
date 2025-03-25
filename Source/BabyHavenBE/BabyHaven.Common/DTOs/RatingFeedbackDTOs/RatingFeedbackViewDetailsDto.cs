2cb69cfae368ab80e7219cb13ff8d90fd3034497
Huynh Nguyen Ngoc Nguyen - N1 <Nguyen1.24112003@gmail.com>
2025-03-25 13:04:37 +0700

    RatingFeedbackDTOs

---
+++ Source/BabyHavenBE/BabyHaven.Common/DTOs/RatingFeedbackDTOs/RatingFeedbackViewDetailsDto.cs
@@ -0,0 +1,34 @@
+﻿using BabyHaven.Common.Enum.RatingFeedbackEnums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace BabyHaven.Common.DTOs.RatingFeedbackDTOs
+{
+    public class RatingFeedbackViewDetailsDto
+    {
+        public Guid UserId { get; set; }
+
+        public int ResponseId { get; set; }
+
+        public int Rating { get; set; }
+
+        public string Comment { get; set; } = string.Empty;
+
+        public DateTime FeedbackDate { get; set; }
+
+        //RatingFeedback Status
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public RatingFeedbackType FeedbackType { get; set; } = RatingFeedbackType.General;
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public FeedbackStatus Status { get; set; } = FeedbackStatus.Pending;
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+    }
+}
