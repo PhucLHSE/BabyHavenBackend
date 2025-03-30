2c822b0f04b49ed6fd6cabffe8d0f7f77317d323
huynh <huynhthaitu124@gmail.com>
2025-03-30 19:57:26 +0700

    Modify logic of alert

--- Source/BabyHavenBE/BabyHaven.Services/Services/AlertService.cs
+++ Source/BabyHavenBE/BabyHaven.Services/Services/AlertService.cs
@@ -226,7 +226,7 @@ namespace BabyHaven.Services.Services
                 bool isBmiIncreasing = IsIncreasingTrend(bmiValues);
                 bool isSuddenBmiChange = IsSuddenChange(bmiValues, 20.0); // Ngưỡng 20%
                 double latestBmi = bmiValues.First();
-                double averageBmi = bmiValues.Average();
+                double averageBmi = bmiValues.First();
 
                 // Tính xu hướng chiều cao
                 var heights = recentRecords.Select(r => r.Height).ToList();
