6c9c60bbb849f636c4cc6143e7d6254f379d7b63
huynh <huynhthaitu124@gmail.com>
2025-03-30 18:05:44 +0700

    Modify attachments

--- Source/BabyHavenBE/BabyHaven.Services/Mappers/ConsultationRequestMapper.cs
+++ Source/BabyHavenBE/BabyHaven.Services/Mappers/ConsultationRequestMapper.cs
@@ -30,7 +30,6 @@ namespace BabyHaven.Services.Mappers
 
                 RequestDate = model.RequestDate,
 
-                Attachments = model.Attachments,
 
                 Description = model.Description,
 
